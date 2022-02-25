fec7b82e55e4622e9d8801bb91e121cbcb8fed02
Eduardo Escobar <eduarddo.escobar@gmail.com>
2022-02-24 19:03:16 -0500

    Made all the seed files

--- data/seeds/02-moves.js
+++ data/seeds/02-moves.js
@@ -1,13 +1,8 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex('moves').insert([
+    {move_name: 'Hydro Pump', move_type: 'Water'},
+    {move_name: 'Flamethrower', move_type: 'Fire'},
+    {move_name: 'Giga Drain', move_type: 'Grass'}
+  ]);
 };
--- data/seeds/03-pokemon-moves.js
+++ data/seeds/03-pokemon-moves.js
@@ -1,13 +1,8 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex('pokemon_moves').insert([
+    {pokemon_id: 1, move_id: 2},
+    {pokemon_id: 2, move_id: 1},
+    {pokemon_id: 3, move_id: 3}
+  ]);
 };
